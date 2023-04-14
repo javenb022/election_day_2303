3579560867caaa035ce2f6d7456266e21e38cfdd
Javen Wilson <javenb022@gmail.com>
2023-04-14 12:16:14 -0400

    Add register_candidate method

--- lib/race.rb
+++ lib/race.rb
@@ -1,6 +1,14 @@
 class Race
-  attr_reader :political_race
+  attr_reader :office,
+              :candidates
   def initialize(political_race)
-    @office = political_race
+    @office     = political_race
+    @candidates = []
+  end
+
+  def register_candidate!(candidate)
+    new_candidate = Candidate.new(candidate)
+    @candidates << new_candidate
+    new_candidate
   end
 end
